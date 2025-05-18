5e1b359376974ff796bb0293c6239e7d223ed175
Samet-Hodaman <samethodaman1@gmail.com>
2025-05-18 17:45:57 +0300

    Dillerin çevirilmeme sorunu düzeltildi

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import App from './App.jsx'
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js';
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
